9eae64ce4e96981c8959380c089ea5439b73bd6b
hamid141215 <eng.hamid.b.youssouf@gmail.com>
2026-01-28 21:31:37 +0300

    Fix conflicts and integrate new template system

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/api/send', authenticate, async (req, res) => {
 const messageOptions = {
     messagingServiceSid: MESSAGING_SERVICE_SID,
     to: `whatsapp:+${cleanPhone}`,
-    contentSid: 'HX42990e73a87738bbb9618d1b9d218f20', 
+    contentSid: 'HXfac5e63d161f07e3ebc652a9931ce1c2', 
     contentVariables: JSON.stringify({ 
         "1": String(req.clientData.name).trim(), 
         "2": String(req.clientData.googleLink).trim() 
